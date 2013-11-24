dd11e8631328cea371153f843a631011dbe28329
Jem Mawson <jem.mawson@gmail.com>
2013-11-24 12:52:48 +0800

    Added blackjacktest class

---
+++ blackjack/BlackjackTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class BlackjackTest extends PHPUnit_Framework_TestCase {
+
+    /*
+     * Complete this test class using the TDD methodology. Red, Green, Refactor.
+     * After discussing with the user's proxy, the acceptance criteria on this story are:
+     *
+     * 1. When no cards are added the score is zero
+     * 2. When a number card is added the score is incremented by that number
+     * 3. When a face card is added the score is incremented by 10
+     * 4. When an ace is added and the score is <= 10, the score is incremented by 11.
+     * 5. When an ace is added and the score is >= 11, the score is incremented by 1.
+     */
+
+}
